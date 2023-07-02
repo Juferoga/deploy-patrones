fdd450ac58ae1a0a92d10eb39043602d9883bd19
Juferoga <juancholoco2000.61@gmail.com>
2023-07-02 00:49:46 -0500

    DEPLOY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,16 @@
 export const environment = {
-  docs:'https://juferoga.github.io/fis',
+  docs:'https://juferoga.github.io/patrones',
   cronograma: 'https://github.com/users/Juferoga/projects/2/views/3',
-  repositorio: 'https://github.com/juferoga/fis',
+  repositorio: 'https://github.com/juferoga/patrones',
   juferoga: 'https://github.com/juferoga/',
   integrantes: [
-    {'name':'Felipe Martin','code':'2018102000','url':'https://github.com/juferoga'},
     {'name':'Brayan Yate','code':'20171020000','url':'https://github.com/juferoga'},
-    {'name':'Nicolas Farias','code':'20181020000','url':'https://github.com/juferoga'},
-    {'name':'Juan Felipe Rodríguez','code':'20181020158','url':'https://github.com/juferoga'},
+    {'name':'Juan Felipe Rodríguez','code':'20181020158','url':'https://github.com/juferoga'}
   ],
   materia: 'PATRONES',
-  proyecto: 'Cine Pacho',
-  profesor: 'Santiago Salazar',
-  api: 'http://localhost:8000/',
+  proyecto: 'proyecto',
+  profesor: 'Helio Ramirez',
+  api: 'http://localhost:8000/api/',
   api_factura: 'http://localhost:8000/',
   server: 'http://localhost:8000/',
 };
